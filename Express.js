68e32906d5219a2b2ccea3d9072e246a5c2c939f
Kundan Kumar Sahu <espkundan@gmail.com>
2023-02-03 18:06:32 +0530

    practice

--- Express.js
+++ Express.js
@@ -4,9 +4,23 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  console.log(req);
+  // res.send('Hello World!');
+  res.send('<h1>Hello World!</h1>')
+});
+
+app.get('/contact', (req, res) => {
+  res.send('Contact me at: espkundan@gmail.com')
+});
+
+app.get('/about', (req, res) => {
+  res.send('I am Flash')
+});
+
+app.get('/api', (req, res) => {
+  res.send('Flash API')
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
